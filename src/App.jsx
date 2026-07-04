63ffa4e3694fc4083fd3056f6553529c06d4c2ed
hasanov.h <metaitschool448@gmail.com>
2026-07-04 18:53:12 +0500

    fghjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
+import './Asilbek/Asilbek.css'
+import logo from './logo.png'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 import Firdavs from './Firdavs/Firdavs'
 import Mrizo from './Mrizo/Mrizo'
-import Asilbek from './Asilbek/Asilbek'
 
-const App = () => {
+const Asilbek = () => {
   return (
+    <BrowserRouter>
     <div>
-      <Asilbek />
+        <div className="Menu">
+            <div className="navbar">
+                <img src={logo} alt="" />
+                <ul>
+                    <li>
+                        <a href="">menu</a>
+                        <Link to="firdavs">news</Link>
+                        <Link to="Umar">about us</Link>
+                        <Link to="Jasurbek">contact us</Link>
+                        <Link to="Samandar"> <button>Log in</button> </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="orta">
+                <div className="orta_t">
+                    <p>Home</p>
+                    <p>|</p>
+                    <p>Menu</p>
+                </div>
+            </div>
+            <Router>
+                <Route path='firdavs' element={<Firdavs />}/>
+                <Route path='Mrizo' element={<Mrizo />}/>
+            </Router>
+        </div>
+
     </div>
+
+
+    </BrowserRouter>
   )
 }
 
-export default App
+export default Asilbek
--- src/Asilbek/Asilbek.jsx
+++ src/Asilbek/Asilbek.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
 import './Asilbek.css'
 import logo from './logo.png'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
+import Firdavs from '../Firdavs/Firdavs'
+import Mrizo from '../Mrizo/Mrizo'
 
 const Asilbek = () => {
   return (
+    <BrowserRouter>
     <div>
-        <div className="tepa_A">
+        <div className="Menu">
             <div className="navbar">
                 <img src={logo} alt="" />
                 <ul>
                     <li>
-                        <a href="/">menu</a>
-                        <a href="Firdavs.jsx">news</a>
-                        <a href="Umar.jsx">about us</a>
-                        <a href="Jasurbek">contact us</a>
-                        <a href="Samandar"> <button>Log in</button> </a>
+                        <Link to="">menu</Link>
+                        <Link to="Firdavs.jsx">news</Link>
+                        <Link to="Umar.jsx">about us</Link>
+                        <Link to="Jasurbek.jsx">contact us</Link>
+                        <Link to="Samandar.jsx"> <button>Log in</button> </Link>
                     </li>
                 </ul>
             </div>
+            <div className="orta">
+                <div className="orta_t">
+                    <p>Home</p>
+                    <p>|</p>
+                    <p>Menu</p>
+                </div>
+            </div>
+            <Router>
+                <Route path='/' element={<Asilbek />}/>
+                <Route path='Firdavs.jsx' element={<Firdavs />}/>
+                <Route path='Mrizo' element={<Mrizo />}/>
+            </Router>
         </div>
+
     </div>
+
+
+    </BrowserRouter>
   )
 }
 
