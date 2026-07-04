7edd63f1e6e8e37d4d510f795a83bf65aae8f2b1
Firdavs0409 <firdavsnumonov2011@gmail.com>
2026-07-04 17:50:53 +0500

    rfyh

--- src/Firdavs/Firdavs.jsx
+++ src/Firdavs/Firdavs.jsx
@@ -1,15 +1,109 @@
 import React from 'react'
+import rasm1 from './firdavs.jpg'
 import './Firdavs.css'
 const Firdavs = () => {
-  return (
-    <div>
-        <div className="news">
+    return (
+        <div>
+
             <ul>
-                <li>home | <span>news</span></li>
+                <li><span>Home</span> | <span>News</span></li>
             </ul>
+            <center>
+                <h1>News</h1>
+            </center>
+
+
+            <div className="asos">
+
+                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>                <div className="card">
+                    <img src={rasm1} alt="" />
+                    <h2 className='v1'>Tips for prepping and caring for your grill</h2>
+                    <p className='v2' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                    <div className="card1">
+
+                        <p className='v3'>Read More</p>
+                        <p className='v4'>16 Apr 2021</p>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-    </div>
-  )
+
+    )
 }
 
 export default Firdavs
