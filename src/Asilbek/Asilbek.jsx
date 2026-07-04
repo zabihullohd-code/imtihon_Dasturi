089579116b22cff59e6e7bfdecb5e799791fd961
hasanov.h <metaitschool448@gmail.com>
2026-07-04 17:58:37 +0500

    efr

--- src/Asilbek/Asilbek.jsx
+++ src/Asilbek/Asilbek.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Asilbek.css'
+import logo from './logo.png'
 
 const Asilbek = () => {
   return (
     <div>
+        <div className="tepa_A">
+            <div className="navbar">
+                <img src={logo} alt="" />
+                <ul>
+                    <li>
+                        <a href="/">menu</a>
+                        <a href="Firdavs.jsx">news</a>
+                        <a href="Umar.jsx">about us</a>
+                        <a href="Jasurbek">contact us</a>
+                        <a href="Samandar"> <button>Log in</button> </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }
