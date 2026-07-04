9bf4c6ea810dadd090770703d4fca3281fb97992
Firdavs0409 <firdavsnumonov2011@gmail.com>
2026-07-04 16:50:25 +0500

    sss

---
+++ src/Firdavs/Firdavs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Firdavs.css'
+const Firdavs = () => {
+  return (
+    <div>
+        <div className="news">
+            <ul>
+                <li>home | <span>news</span></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Firdavs
