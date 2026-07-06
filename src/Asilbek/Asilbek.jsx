c45039be1fcff3b2107c652c37e377d9e0f7a7d2
metaitschool448 <zabihullohd@gmail.com>
2026-07-06 18:36:57 +0500

    dfghjkhgfdghjkhgfdfghjgfdghjgfdg

--- src/Asilbek/Asilbek.jsx
+++ src/Asilbek/Asilbek.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Asilbek.css'
+import img1 from './imgg.png'
 import logo from './logo.png'
 import { BrowserRouter, Route, Router } from 'react-router-dom'
 import Firdavs from '../Firdavs/Firdavs'
@@ -16,96 +17,147 @@ const Asilbek = () => {
                             <p>|</p>
                             <p className='asd'>Menu</p>
                         </div>
-                        <div className="chap">
-                            <h2>Our Regular Menu Pack</h2>
-                            <div className="tugma">
-                                <a href="">
-                                    <button>all</button>
-                                </a>
-                                <a href="">
-                                    <button>kebabs</button>
-                                </a>
-                                <a href="">
-                                    <button>pidesburgers</button>
-                                </a>
-                                <a href="">
-                                    <button>other dishes</button>
-                                </a>
-                                <a href="">
-                                    <button>sides</button>
-                                </a>
-                                <a href="">
-                                    <button>drinks</button>
-                                </a>
-                                <a href="">
-                                    <button>desserts</button>
-                                </a>
-                                <a href="">
-                                    <button>meal deals</button>
-                                </a>
+                        
+                            <div className="chap">
+                                <h2>Our Regular Menu Pack</h2>
+                                <div className="tugma">
+                                    <a href="">
+                                        <button>all</button>
+                                    </a>
+                                    <a href="">
+                                        <button>kebabs</button>
+                                    </a>
+                                    <a href="">
+                                        <button>pidesburgers</button>
+                                    </a>
+                                    <a href="">
+                                        <button>other dishes</button>
+                                    </a>
+                                    <a href="">
+                                        <button>sides</button>
+                                    </a>
+                                    <a href="">
+                                        <button>drinks</button>
+                                    </a>
+                                    <a href="">
+                                        <button>desserts</button>
+                                    </a>
+                                    <a href="">
+                                        <button>meal deals</button>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
+                            <div className="ong">
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img">
+                                    <img src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div  className="img1">
+                                    <img className='qwer' src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                <div className="img1">
+                                    <img className='qwer' src={img1} alt="" />
+                                    <h4>Barbecue Shish kebab Shashlik <br /> Skewer</h4>
+                                    <p>$12.00</p>
+                                </div>
+                                
+                            </div>
+                      
                     </div>
                 </div>
-                <div className="pass">
-                    <div className="footer11">
+                <div className="footer11">
 
-                        <div className="shriftlar">
+                    <div className="shriftlar">
+                    <br /><br /><br /><br /><br /><br /><br />
 
-                            <h2>Newsletter</h2>
+                        <h2>Newsletter</h2>
 
-                            <h1>Subscribe to Our Newsletter</h1>
+                        <h1>Subscribe to Our Newsletter</h1>
 
-                            <p>And never miss latest Updates!</p>
+                        <p>And never miss latest Updates!</p>
 
-                        </div>
-
-                        <div className="inpbtn">
-
-                            <input type="text" placeholder='Email Address' />
-
-                            <button>Subscribe</button>
-                        </div>
                     </div>
 
+                    <div className="inpbtn">
 
+                        <input type="text" placeholder='Email Address' />
 
-                    {/* foteer1 */}
-
-
-
-
-                    <div className="footer21">
-                        <div className="a1">
-                            <h2>Contact Us</h2>
-                            <br />
-                            <p>9 W 53rd St, London, NY 10019, UK</p>
-                            <br />
-                            <p>+1 212-344-1230 <br /> +1 212-555-1230</p>
-                        </div>
-
-                        <div className="a2">
-                            <img src={logo} alt="" />
-                            <br />
-                            <p>"The best way to find yourself is to lose yourself in the service of others.”</p>
-                            <br />
-                            <div className="i">
-                                <i class="fa-brands fa-facebook-f"></i>
-                                <i class="fa-brands fa-twitter"></i>
-                                <i class="fa-brands fa-instagram"></i>
-                            </div>
-                        </div>
-
-                        <div className="a3">
-                            <h2>Working Hours</h2>
-                            <br />
-                            <p>Monday-Friday: <br /> 08:00 am -12:00 am</p>
-                            <br />
-                            <p>Saturday-Sunday: <br /> 07:00am -11:00 pm</p>
-                        </div>
+                        <button>Subscribe</button>
                     </div>
                 </div>
-            </div>
+
+
+                {/* foteer1 */}
+
+
+                <div className="footer21">
+                    <div className="a1">
+                        <h2>Contact Us</h2>
+                        <br />
+                        <p>9 W 53rd St, London, NY 10019, UK</p>
+                        <br />
+                        <p>+1 212-344-1230 <br /> +1 212-555-1230</p>
+                    </div>
+
+                    <div className="a2">
+                        <img src={logo} alt="" />
+                        <br />
+                        <p>"The best way to find yourself is to lose yourself in the service of others.”</p>
+                        <br />
+                        <div className="i">
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                    </div>
+
+                    <div className="a3">
+                        <h2>Working Hours</h2>
+                        <br />
+                        <p>Monday-Friday: <br /> 08:00 am -12:00 am</p>
+                        <br />
+                        <p>Saturday-Sunday: <br /> 07:00am -11:00 pm</p>
+                    </div>
+                </div>
+                <br /><br /><br /><br /><br /><br /><br /><br />            </div>
 
         </div>
 
